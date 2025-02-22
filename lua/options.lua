eccdaf35afaaa55a8f2a55751cf944a52da2ea5e
Rienour <r.d.eichenour@gmail.com>
2025-02-22 16:11:55 -0600

    [UPDT] - Move vim options to dedicated file
    In order ot allow Nevim to find the options set when working in a
    directory outside the the pwd the options need to be moved into the lua
    directory.
    
    - Move vim options into `lua/options.lua`
    
    Signed-off-by: Rienour <r.d.eichenour@gmail.com>

---
+++ lua/options.lua
@@ -0,0 +1,36 @@
+-- Add line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Make splits occur ot the right and below
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Update tab behavior
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+
+-- Update Neovim to interact with the system clipboard through the unnamedplus register
+vim.opt.clipboard = "unnamedplus"
+
+-- Cursor options
+vim.opt.cursorline = true
+
+-- Highlight options
+vim.opt.hlsearch = false
+
+-- Visual Block Adjustments
+vim.opt.virtualedit = "block"
+
+-- Session Creation
+vim.opt.sessionoptions = word,blank,buffers,folds,localoptions,options,resize,tabpages,winsize
+
+-- Enable wider range of colors
+vim.opt.termguicolors = true
+
+-- Setup leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
