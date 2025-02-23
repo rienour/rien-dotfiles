2083e6dff2550fa9ac2e63384724883ef2c76bb8
Rienour <r.d.eichenour@gmail.com>
2025-02-23 17:19:16 -0600

    [INST] - Install and configure conform.nvim
    In order to better support formatting based on user preferences, the
    conform plugin will be installed and configured.
    
    - Install `conform.nvim` plugin
    
    Signed-off-by: Rienour <r.d.eichenour@gmail.com>

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			format_on_save = {
+				timeout_ms = 500,
+			},
+		},
+	},
+}
