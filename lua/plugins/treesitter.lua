57fbd6c0bfc358a4c26583cbddba58cb9ba35cf1
Rienour <r.d.eichenour@gmail.com>
2025-02-22 18:07:45 -0600

    [INST] - Add nvim-treesitter and textobjects
    In order to support better syntax highlighting and selection the
    treesitter pluging and textobjects sub plugin will be added.
    
    - Install and configure `nvim-treesitter`
    - Install and configure `nvim-treesitter-textobjects`
    
    Signed-off-by: Rienour <r.d.eichenour@gmail.com>

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {},
+      },
+      auto_install = false,
+      highlight = {
+        enabled = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'v', -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+}
