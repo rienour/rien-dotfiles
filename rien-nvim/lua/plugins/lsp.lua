155eb717f9da2b7fc90e1dc341f5354079a00aab
rienour <R.D.Eichenour@gmail.com>
2025-06-18 21:08:24 -0500

    [CONF] - Update LuaLS settings in LSP
    In order to resolve issues about the undefined vim global and remoe
    telemetry, the LuaLS settings will be updated to resolve the issues.
    
    - Add settings table to lua_ls config setup call
    
    Signed-off-by: rienour <R.D.Eichenour@gmail.com>

--- rien-nvim/lua/plugins/lsp.lua
+++ rien-nvim/lua/plugins/lsp.lua
@@ -3,7 +3,21 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            telemetry = {
+              enalbled = false,
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+          },
+        },
+      })
     end,
   },
 }
