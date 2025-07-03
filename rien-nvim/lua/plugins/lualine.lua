b59379e15a48a096cabec55a04bde66bd74563d3
rienour <R.D.Eichenour@gmail.com>
2025-07-02 21:40:37 -0500

    [INST] - Add lualine to rien-nvim configuration
    The lualine plugin has been installed and configured as a baseline based
    on developer preference.
    
    - Install lualine.nvim
    
    Signed-off-by: rienour <R.D.Eichenour@gmail.com>

---
+++ rien-nvim/lua/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "lsp_status" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = { "diagnostics" },
+      },
+      tabline = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_x = { "diagnostics", "filename" },
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+      },
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    },
+  },
+}
