3bb1ce3ddad97a254b9f9c76a8baa95c947844b3
Eichenour, Riley <R.D.Eichenour@gmail.com>
2025-11-18 18:57:32 -0600

    [RFCT] - Refactor LSP config to simplify configuration
    Signed-off-by: Eichenour, Riley <R.D.Eichenour@gmail.com>

--- rien-nvim/lua/plugins/lsp.lua
+++ rien-nvim/lua/plugins/lsp.lua
@@ -1,24 +1,32 @@
+-- This table will contain the mapping for the LSP server to the settings,
+-- if there are any, alongside the nvim-lspconfig
+local lsp_configs = {
+  lua_ls = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      telemetry = {
+        enalbled = false,
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
+}
+
 return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      vim.lsp.config("lua_ls", {
-        on_attach = require("util.lsp").on_client_attach,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-            telemetry = {
-              enalbled = false,
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-          },
-        },
-      })
-      vim.lsp.enable({ "lua_ls" })
+      for name, settings in pairs(lsp_configs) do
+        vim.lsp.config(name, {
+          on_attach = require("util.lsp").on_client_attach,
+          settings = settings,
+        })
+        vim.lsp.enable({ name })
+      end
     end,
   },
 }
